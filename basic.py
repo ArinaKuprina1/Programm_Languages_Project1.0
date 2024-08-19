3f83d527d2d317806367181e38423d8bbcc12fe3
ShayGelbart <shaygelbart1@gmail.com>
2024-08-19 22:57:24 +0300

    changes to parser

--- basic.py
+++ basic.py
@@ -18,20 +18,19 @@ class Error:
         self.details = details
 
     def as_string(self):
-        result = f'{self.error_name}: {self.details}\n'
-        result += f'File {self.pos_start.fname}, line {self.pos_start.line + 1}'
+        result = f'{self.error_name}: {self.details}'
+        result += f'File {self.pos_start.fname}, line{self.pos_start.line + 1}'
         result += '\n\n' + string_with_arrows(self.pos_start.ftext, self.pos_start, self.pos_end)
         return result
 
 
-class IllegalCharError(Error):
+class IllegalCharacterError(Error):
     def __init__(self, pos_start, pos_end, details):
         super().__init__(pos_start, pos_end, 'Illegal Character', details)
 
-
 class InvalidSyntaxError(Error):
-    def __init__(self, pos_start, pos_end, details=''):
-        super().__init__(pos_start, pos_end, 'Invalid Syntax', details)
+        def __init__(self, pos_start, pos_end, details=''):
+                super().__init__(pos_start, pos_end, 'Invalid Syntax', details)
 
 
 ###############
